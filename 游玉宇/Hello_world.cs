c2c53f9680eca05f17f509f3946301e512cb3ff4
Yinlianlei <57148635+Yinlianlei@users.noreply.github.com>
2019-11-04 23:53:39 +0800

    Add files via upload

---
+++ 游玉宇/Hello_world.cs
@@ -0,0 +1,11 @@
+using System;
+namespace hellp_world_C
+{
+    class hellp_world
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello_world\n");
+        }
+    }
+}
